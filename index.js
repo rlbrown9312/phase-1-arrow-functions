f2357a5ddbc0730150ca7b413776df26ddad4615
Richard <richard.brown0807@gmail.com>
2022-08-09 09:35:38 -0500

    Done with assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1, num2) {
+   num1 = 2000;
+   num2 = 100; 
+    return num1 / num2;
+}
+
+const square = num1 => num1 ** 2
+
+const add = (num1, num2) => num1 + num2
+
+console.log(add(7, 10));
